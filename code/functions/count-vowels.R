69f3d3f97c7f1de1d3642cfc9cdccdff58a2651d
Cora Bai <baiyt.cora2021@berkeley.edu>
2018-11-03 14:39:23 -0700

    count vowels function

---
+++ code/functions/count-vowels.R
@@ -0,0 +1,13 @@
+library("stringr")
+count_vowels = function(x){
+  if(!(is.character(x) && length(x) ==1)){
+    stop("invalid input; a string was expected")
+  }
+  else{
+    x = casefold(x, upper = FALSE)
+    vec = c(str_count(x, pattern = "a"), str_count(x, pattern = "e"), str_count(x, pattern = "i"), str_count(x, pattern = "o"), str_count(x, "u"))
+    vec = as.double(vec)
+    names(vec) = c("a","e","i","o","u")
+  }
+  return(vec)
+}
