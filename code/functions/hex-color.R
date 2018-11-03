d287272c5a220103fedab9c92abb766a1930e3d4
Cora Bai <baiyt.cora2021@berkeley.edu>
2018-11-03 01:23:00 -0700

    alpha function update

--- code/functions/hex-color.R
+++ code/functions/hex-color.R
@@ -7,3 +7,12 @@ is_hex = function(x){
   return(grepl("^#(\\d|[a-f]){6}$", x, ignore.case = TRUE))
   }
 }
+
+is_hex_alpha = function(x){
+  if(!(is.character(x) && length(x) ==1)){
+    stop("invalid input; a string was expected")
+  }
+  else{
+    return(grepl("^#(\\d|[a-f]){8}$", x, ignore.case = TRUE))
+  }
+}
