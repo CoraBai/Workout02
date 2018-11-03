d7e8dfc5547295e96d2d3b81c44d6191e4d41f49
Cora Bai <baiyt.cora2021@berkeley.edu>
2018-11-03 01:17:18 -0700

    test-hex

---
+++ code/tests/test-hex-color.R
@@ -0,0 +1,16 @@
+library("testthat")
+source("../functions/hex-color.R")
+context("Test for is-hex-color")
+
+test_that("is_hex() works as expected", {
+  expect_true(is_hex("#FF00A7"))
+  expect_true(is_hex("#ff0000"))
+  expect_true(is_hex("#123456"))
+  expect_true(is_hex("#12Fb56"))
+  expect_false(is_hex("FF0000"))
+  expect_false(is_hex("#1234GF"))
+  expect_false(is_hex("#1234567"))
+  expect_false(is_hex("blue"))
+  expect_error(is_hex(FF00A7))
+  expect_error(is_hex(TRUE))
+})
